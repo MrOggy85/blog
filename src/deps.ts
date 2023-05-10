a499c14413a241f1a72735edf876befef4b88106
Oskar Okuno <2677573+MrOggy85@users.noreply.github.com>
2023-05-10 10:17:23 +0900

    chore: update deps

--- src/deps.ts
+++ src/deps.ts
@@ -1,14 +1,14 @@
-export { CSS as GitHub_Flavored_Markdown_CSS  } from "https://deno.land/x/gfm@0.1.26/mod.ts";
+export { CSS as GitHub_Flavored_Markdown_CSS  } from "https://deno.land/x/gfm@0.2.2/mod.ts";
 export {
   Application,
   Context,
   helpers,
   Router,
-} from "https://deno.land/x/oak@v11.1.0/mod.ts";
+} from "https://deno.land/x/oak@v12.4.0/mod.ts";
 
-export type { RouterContext } from "https://deno.land/x/oak@v11.1.0/mod.ts";
+export type { RouterContext } from "https://deno.land/x/oak@v12.4.0/mod.ts";
 
-export { parse } from "https://deno.land/std@0.163.0/datetime/mod.ts";
+export { parse } from "https://deno.land/std@0.186.0/datetime/mod.ts";
 
 export { Feed } from "https://esm.sh/feed@4.2.2";
 
@@ -16,4 +16,4 @@ export * as Marked from "https://esm.sh/marked@4.0.12";
 export { default as Prism } from "https://esm.sh/prismjs@1.27.0";
 export { default as sanitizeHtml } from "https://esm.sh/sanitize-html@2.7.0";
 export { escape as htmlEscape } from "https://esm.sh/he@1.2.0";
-export { emojify } from "https://deno.land/x/emoji@0.1.2/mod.ts";
+export { emojify } from "https://deno.land/x/emoji@0.3.0/mod.ts";
