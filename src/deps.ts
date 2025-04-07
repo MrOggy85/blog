1638bfa45204caa15413bd2d2b9cef1d55c7be36
Oskar Okuno <2677573+MrOggy85@users.noreply.github.com>
2025-04-07 18:59:04 +0900

    chore: update deps types

--- src/deps.ts
+++ src/deps.ts
@@ -1,4 +1,4 @@
-export { CSS as GitHub_Flavored_Markdown_CSS } from 'https://deno.land/x/gfm@0.2.2/mod.ts';
+export { CSS as GitHub_Flavored_Markdown_CSS } from 'jsr:@deno/gfm';
 export {
 	Application,
 	Context,
@@ -10,10 +10,16 @@ export type { RouterContext } from 'https://deno.land/x/oak@v12.4.0/mod.ts';
 
 export { parse } from 'https://deno.land/std@0.186.0/datetime/mod.ts';
 
-export { Feed } from 'https://esm.sh/feed@4.2.2';
+export { Feed } from 'npm:feed@4.2.2';
 
-export * as Marked from 'https://esm.sh/marked@4.0.12';
-export { default as Prism } from 'https://esm.sh/prismjs@1.27.0';
-export { default as sanitizeHtml } from 'https://esm.sh/sanitize-html@2.7.0';
-export { escape as htmlEscape } from 'https://esm.sh/he@1.2.0';
-export { emojify } from 'https://deno.land/x/emoji@0.3.0/mod.ts';
+// @ts-types="npm:@types/marked@4.0.1"
+import * as Marked from 'npm:marked@4.0.12';
+export { Marked };
+export { default as Prism } from 'npm:prismjs@1.27.0';
+export { default as sanitizeHtml } from 'npm:sanitize-html@2.7.0';
+
+import he from 'npm:he@1.2.0';
+const htmlEscape = he.escape;
+export { htmlEscape };
+
+export { emojify } from 'jsr:@denosaurs/emoji';
